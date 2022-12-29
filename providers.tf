46ee23723be544c85c05c84aec54e8d9d6ca6848
unknown <abhibaikan@gmail.com>
2022-12-29 23:10:00 +0530

    removed accesskeys

--- providers.tf
+++ providers.tf
@@ -6,10 +6,3 @@ terraform {
     }
   }
 }
-
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-west-2"
-  access_key = "AKIAYG7VXYLM4EUJRDGC"
-  secret_key = "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"
-}
